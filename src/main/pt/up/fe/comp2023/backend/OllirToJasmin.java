88894f4215eab298ff6b52dac71f86634422d59b
JoAPSo de Oliveira Gigante Pinheiro <up202008133@fe.up.pt>
2023-05-27 21:32:44 +0100

    better optimization lt

--- src/main/pt/up/fe/comp2023/backend/OllirToJasmin.java
+++ src/main/pt/up/fe/comp2023/backend/OllirToJasmin.java
@@ -309,18 +309,24 @@ public class OllirToJasmin {
         boolean rightIsLiteral = right.isLiteral();
         boolean leftIsLiteral = left.isLiteral();
 
-            jasminCodeBuilder.append(JasminUtils.loadElement(method, left));
-            jasminCodeBuilder.append("\n\t");
-
-            jasminCodeBuilder.append(JasminUtils.loadElement(method, right));
-            jasminCodeBuilder.append("\n\t");
-
-
-        if(!(instruction.getOperation().getOpType() == OperationType.ADD ||
+        boolean mathOp = instruction.getOperation().getOpType() == OperationType.ADD ||
                 instruction.getOperation().getOpType() == OperationType.SUB ||
                 instruction.getOperation().getOpType() == OperationType.MUL ||
-                instruction.getOperation().getOpType() == OperationType.DIV)) {
+                instruction.getOperation().getOpType() == OperationType.DIV;
+
+        if (!(leftIsLiteral && ((LiteralElement) left).getLiteral().equals("0") && !mathOp)) {
+            jasminCodeBuilder.append(JasminUtils.loadElement(method, left));
+            jasminCodeBuilder.append("\n\t");
+        }
+        if (!(rightIsLiteral && ((LiteralElement) right).getLiteral().equals("0") && !mathOp)) {
+            jasminCodeBuilder.append(JasminUtils.loadElement(method, right));
+            jasminCodeBuilder.append("\n\t");
+        }
+
+        if(!mathOp) {
             boolean oneIsZero = false;
+            if (leftIsLiteral && ((LiteralElement) left).getLiteral().equals("0")) oneIsZero = true;
+            else if (rightIsLiteral && ((LiteralElement) right).getLiteral().equals("0")) oneIsZero = true;
 
             if (oneIsZero) jasminCodeBuilder.append("if");
             else jasminCodeBuilder.append("if_icmp");
