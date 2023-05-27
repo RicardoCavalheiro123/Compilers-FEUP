d2d80a9552c962f9c32f50a9bca66729339aca1b
Zé Araújo <72151399+zearaujo11@users.noreply.github.com>
2023-05-27 16:21:49 +0100

    another try to fix

--- src/main/pt/up/fe/comp2023/optimization/ConstantFoldingVisitor.java
+++ src/main/pt/up/fe/comp2023/optimization/ConstantFoldingVisitor.java
@@ -64,6 +64,7 @@ public class ConstantFoldingVisitor extends PreorderJmmVisitor<Boolean, Boolean>
 
         if(node.getJmmChild(0).getKind().equals("Identifier") || node.getJmmChild(1).getKind().equals("Identifier") ||
         node.getJmmChild(0).getKind().equals("BinaryOp") || node.getJmmChild(1).getKind().equals("BinaryOp") ||
+        node.getJmmChild(0).getKind().equals("UnaryOp") || node.getJmmChild(1).getKind().equals("UnaryOp") ||
         node.getJmmChild(0).getKind().equals("Parenthesis") || node.getJmmChild(1).getKind().equals("Parenthesis")) return false;
 
         var v1 = node.getJmmChild(0).get("value");
