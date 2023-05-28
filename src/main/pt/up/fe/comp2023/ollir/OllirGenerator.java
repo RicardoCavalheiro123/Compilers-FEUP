d334609ab2983014b6ba077169b450cffb4fbd18
JoAPSo de Oliveira Gigante Pinheiro <up202008133@fe.up.pt>
2023-05-28 06:51:38 +0100

    fix more

--- src/main/pt/up/fe/comp2023/ollir/OllirGenerator.java
+++ src/main/pt/up/fe/comp2023/ollir/OllirGenerator.java
@@ -318,7 +318,7 @@ public class OllirGenerator extends AJmmVisitor<StringBuilder, String> {
             ex = "temp" + (temp_counter - 1) + "." + jmmNode.get("id");
         }
         this.ollirCode.append("." + jmmNode.get("id") + ",\"<init>\").V");
-        if(!(jmmNode.getJmmParent().getKind().equals("Parenthesis") || jmmNode.getJmmParent().getKind().equals("Assign"))){
+        if(!(jmmNode.getJmmParent().getKind().equals("Parenthesis") )){
             this.ollirCode.append(";\n");
         }
 
