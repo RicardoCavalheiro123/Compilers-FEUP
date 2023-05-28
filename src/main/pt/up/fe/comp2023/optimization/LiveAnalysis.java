eb4c7637b93782e3697d91ea5db65bfeb45b0352
RicardoCavalheiro123 <ricardo18-4-2001@hotmail.com>
2023-05-28 20:03:41 +0100

    in out

--- src/main/pt/up/fe/comp2023/optimization/LiveAnalysis.java
+++ src/main/pt/up/fe/comp2023/optimization/LiveAnalysis.java
@@ -3,10 +3,7 @@ package pt.up.fe.comp2023.optimization;
 import org.specs.comp.ollir.*;
 
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class LiveAnalysis {
 
@@ -32,9 +29,47 @@ public class LiveAnalysis {
                 instructions.add(liveNode);
             }
 
+            computeInOut(instructions);
+
+
         }
 
         return liveAnalysis;
+    }
+
+    public void computeInOut(List<LiveNode> instructions){
+        boolean changes = true;
+        while(changes) {
+            for(LiveNode instruction : instructions) {
+                var in = new HashSet<>(instruction.in);
+                var out = new HashSet<>(instruction.out);
+
+                var newIn = new HashSet<>();
+                var newOut = new HashSet<>();
+                var set = new HashSet<>();
+                set.addAll(instruction.out);
+                set.removeAll(instruction.def);
+                newIn.addAll(set);
+
+
+
+                for(var successor : instruction.instruction.getSuccessors()) {
+                    var liveNode = getLiveNode(instructions, successor);
+                    newOut.addAll(liveNode.in);
+                }
+            }
+        }
+
+
+    }
+
+    public LiveNode getLiveNode(List<LiveNode> instructions, Node node) {
+        for(LiveNode liveNode : instructions) {
+            if(liveNode.instruction.equals(node)) {
+                return liveNode;
+            }
+        }
+        return null;
     }
 
 
